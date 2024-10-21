8bf444ff635d4ca95c964e59347b45130825b5ac
Herman Polloni <pollonih@amazon.com>
2024-10-20 17:03:40 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+### Building
+```sh
+brew install sdl2
+brew install sdl2_ttf
+
+LIBRARY_PATH="$LIBRARY_PATH:/opt/homebrew/lib" cargo build
+```
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::{fs::File, io::Read};
+
+use cpu::CPU;
+
+mod cpu;
+
+fn main() {
+    let mut file = File::open("roms/nestest/nestest.nes").unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+
+    let mut ram = [0u8; 65536];
+
+    ram[0x8000..0xBFFF].copy_from_slice(&buffer[0x0010..0x400f]);
+    ram[0xC000..0xFFFF].copy_from_slice(&buffer[0x0010..0x400f]);
+
+    let mut cpu = CPU::new(0xC000, Box::new(ram));
+
+    // Execute the first 2 instr
+    // This will eventually crash
+    loop {
+        cpu.step();
+    }
+    /*
+    let mut file = File::open("roms/instr_test-v5/01-basics.nes").unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+
+    let mut ram = [0u8; 65536];
+
+    ram[0x8000..0xFFFF].copy_from_slice(&buffer[0x0010..0x800f]);
+
+    let mut cpu = CPU::new(0xFFFC, Box::new(ram));
+
+    cpu.step();*/
+}
